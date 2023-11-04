b01a0fa1ebf21e6723931da2011c4193c0038217
absszerro <cryinghostt.56@gmail.com>
2023-11-04 10:25:00 +0300

    init commit

---
+++ src/main/java/org/abszerro/characterfrequencyanalyzer/CharacterFrequencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package org.abszerro.characterfrequencyanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterFrequencyAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterFrequencyAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/abszerro/characterfrequencyanalyzer/CharacterFrequencyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.abszerro.characterfrequencyanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterFrequencyAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
